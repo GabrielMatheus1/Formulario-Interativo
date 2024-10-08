fc94a8498d0c2c642b764a62206eaf3c72f823e8
Gabriel Matheus <133973057+GabrielMatheus1@users.noreply.github.com>
2024-10-08 12:56:33 -0300

    Update app.js

--- app.js
+++ app.js
@@ -131,3 +131,14 @@ botaoTresFormBack.addEventListener('click', () => {
     stepDois.classList.add('active');
     
 });
+
+var form = document.getElementById('form');
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    fetch(form.action, {
+        method: "POST",
+        body: new FormData(form),
+    }).then(response => response.json())
+    .then(data => {});
+});
